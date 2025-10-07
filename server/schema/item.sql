36ac0457aebd0190ce59c213cc351e9c3826bc58
Thien Pham <45925109+th13n-h4ck3r@users.noreply.github.com>
2025-10-06 22:32:31 -0500

    adding sql create table commands

--- server/schema/item.sql
+++ server/schema/item.sql
@@ -4,6 +4,7 @@ CREATE TABLE ITEM (
     on_hold INT NOT NULL DEFAULT 0,
     loaned_out INT NOT NULL DEFAULT 0,
     quantity INT GENERATED ALWAYS AS (available + on_hold + loaned_out) STORED,
+    thumbnail BLOB,
     earliest_available_date DATE,
     category ENUM('BOOK', 'MOVIE', 'DEVICE')
 );
