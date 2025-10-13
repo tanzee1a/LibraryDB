23e8605810d4049a670edf111121492302140a4d
Alex Melendez <alexamelen.03@gmail.com>
2025-10-13 17:51:02 -0500

    homepage starting point UI
    and some more routing to future pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,22 @@ import { Routes, Route} from 'react-router-dom'
 import HomePage from './pages/home_page/homepage.jsx'
 import Login from './pages/login_page/login.jsx'
 import Notifications from './pages/notifications/notification.jsx'
+import Profile from './pages/profile_page/profile.jsx'
+import Catalog from './pages/catalog_page/catalog.jsx'
 import './App.css'
 
 function App() {
 
   return (
     <Routes>
+      {/* USERS */}
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/notifications" element={<Notifications />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/catalog" element={<Catalog />} />
+
+      {/* ADMIN */}
     </Routes>
   )
 }
---
+++ client/src/pages/catalog_page/catalog.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './catalog.css'
+
+function catalog() {
+
+  return (
+    <div>
+      This is our catalog page
+    </div>
+  )
+}
+
+export default catalog
