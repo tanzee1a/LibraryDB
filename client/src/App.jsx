1eae151e5da6c9f2bcf21fbe341eb377c7705135
Alex Melendez <alexamelen.03@gmail.com>
2025-10-14 00:28:55 -0500

    navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,21 +4,60 @@ import Login from './pages/login_page/login.jsx'
 import Notifications from './pages/notifications/notification.jsx'
 import Profile from './pages/profile_page/profile.jsx'
 import Catalog from './pages/catalog_page/catalog.jsx'
+import Navbar from './navbar_component/navbar.jsx'
 import './App.css'
 
 function App() {
 
   return (
-    <Routes>
-      {/* USERS */}
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/notifications" element={<Notifications />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/catalog" element={<Catalog />} />
+    <>
+      <Routes>
+        {/* USERS */}
+        {/* Homepage may need a different navbar view later*/}
+        <Route path="/" element={
+          <>
+            <Navbar />
+            <div className="main-content">
+              <HomePage />
+            </div>
+          </>
+        } />
+        <Route path="/login" element={
+          <>
+            <Navbar />
+            <div className="main-content">
+              <Login />
+            </div>
+          </>
+        } />
+        <Route path="/notifications" element={
+          <>
+            <Navbar />
+            <div className="main-content">
+              <Notifications />
+            </div>
+          </>
+        } />
+        <Route path="/profile" element={
+          <>
+            <Navbar />
+            <div className="main-content">
+              <Profile />
+            </div>
+          </>
+        } />
+        <Route path="/catalog" element={
+          <>
+            <Navbar />
+            <div className="main-content">
+              <Catalog />
+            </div>
+          </>
+        } />
 
-      {/* ADMIN */}
-    </Routes>
+        {/* ADMIN */}
+      </Routes>
+    </>
   )
 }
 
