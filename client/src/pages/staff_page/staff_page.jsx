bd0701897db6847a1a7a8f1e50aceb074a53e7c6
AirIckBee <ericbretherton@gmail.com>
2025-10-20 12:53:48 -0500

    Initial state upload
    It exists now

---
+++ client/src/pages/staff_page/staff_page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './staff_page.css'
+
+function staff_page() {
+
+  return (
+    <div>
+    This is our staff page
+    </div>
+  )
+}
+
+export default staff_page
