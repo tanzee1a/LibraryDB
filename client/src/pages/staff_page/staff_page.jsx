736488975a65f7fc73066b4cfca8b217454532c8
AirIckBee <ericbretherton@gmail.com>
2025-10-21 14:02:12 -0500

    Update staff_page.jsx
    very basic framework for what the page needs to do. sub pages likely necessary to separate staff's various purposes

--- client/src/pages/staff_page/staff_page.jsx
+++ client/src/pages/staff_page/staff_page.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import './staff_page.css'
 
 function staff_page() {
-
   return (
     <div>
-    This is our staff page
+      <p>This is our staff page</p>
+      <ul>
+        <li><button>Manage Fines</button></li>
+        <li><button>Manage Users</button></li>
+        <li><button>AMnage Items</button></li>
+      </ul>
     </div>
   )
 }
+
 
 export default staff_page
