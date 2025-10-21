8a6b917478d017137c6fc66844910b41bee7c4fd
Thien Pham <45925109+th13n-h4ck3r@users.noreply.github.com>
2025-10-21 16:34:22 -0500

    Comment out global css edit.

--- client/src/pages/catalog_page/item_details.jsx
+++ client/src/pages/catalog_page/item_details.jsx
@@ -19,12 +19,12 @@ function ItemDetails() {
             <hr className="divider" />
             <p>This small, portable book presents a unique perspective on the human body for artists to study and implement in their drawing work. In this book, artist and teacher Michel Lauricella simplifies the human body into basic shapes and forms, offering profound insight for artists of all kinds, sparking the imagination and improving one’s observational abilities. Rather than going the traditional route of memorizing a repertoire of poses, Lauricella instead stresses learning this small collection of forms, which can then be combined and shaped into the more complex and varied forms and postures we see in the living body. Geared toward artists of all levels—from beginners through professionals—this handy, pocket-sized book will help spark your imagination and creativity. Whether your interest is in figure drawing, fine arts, fashion design, game design, or creating comic book or manga art, you will find this helpful book filled with actionable insights.</p>
             <hr className="divider" />
-            <div className="additional-info">
-              <p><strong>Publisher:</strong> Random House Publishing Group</p>
-              <p><strong>Publication Date:</strong> March 3, 2020</p>
-              <p><strong>ISBN:</strong> 9781984881666</p>
-              <p><strong>Pages:</strong> 320</p>
-            </div>
+            <ul className='additional-info'>
+              <li><strong>Publisher:</strong> Random House Publishing Group</li>
+              <li><strong>Publication Date:</strong> March 3, 2020</li>
+              <li><strong>ISBN:</strong> 9781984881666</li>
+              <li><strong>Pages:</strong> 320</li>
+            </ul>
             <hr className="divider" />
           </div>
 
