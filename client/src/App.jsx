408ad3e4b3c5006bebfceded73f8d00805a85c82
Alex Melendez <alexamelen.03@gmail.com>
2025-10-21 14:56:49 -0500

    removing empty profile page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import HomePage from './pages/home_page/homepage.jsx'
 import Login from './pages/login_page/login.jsx'
 import Notifications from './pages/notifications/notification.jsx'
-import Profile from './pages/profile_page/profile.jsx'
 import Catalog from './pages/catalog_page/catalog.jsx'
 import BookCatalog from './pages/catalog_page/book.jsx'
 import MovieCatalog from './pages/catalog_page/movie.jsx'
@@ -22,7 +21,6 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/notifications" element={<Notifications />} />
-        <Route path="/profile" element={<Profile />} />
         <Route path="/item-details" element={<ItemDetails />} />
         <Route path="/catalog" element={<Catalog />} />
         <Route path="/account" element={<AccountDashboard />} />
@@ -30,7 +28,6 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/notifications" element={<Notifications />} />
-        <Route path="/profile" element={<Profile />} />
         <Route path="/catalog" element={<Catalog />} />
         <Route path="/books" element={<BookCatalog />} />
         <Route path="/movies" element={<MovieCatalog />} />
