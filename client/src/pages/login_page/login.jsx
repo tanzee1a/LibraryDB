8e672fbeff994ef2065606867668065ea075f638
tanzee1a <126806377+tanzee1a@users.noreply.github.com>
2025-10-28 16:43:47 -0500

    Update login.jsx

--- client/src/pages/login_page/login.jsx
+++ client/src/pages/login_page/login.jsx
@@ -15,7 +15,7 @@ function Login({ setIsStaff, setIsLoggedIn }) {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:5000/api/login', {
+      const response = await fetch('${API_BASE_URL}/api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password })
