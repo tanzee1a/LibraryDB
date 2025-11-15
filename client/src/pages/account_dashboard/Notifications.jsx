5c4ace688426c935331b4f514f361111276a188f
tanzee1a <126806377+tanzee1a@users.noreply.github.com>
2025-11-15 11:17:15 -0600

    automate mark as read on user side

--- client/src/pages/account_dashboard/Notifications.jsx
+++ client/src/pages/account_dashboard/Notifications.jsx
@@ -80,7 +80,11 @@ function Notifications({ setUnreadCount = () => {} }) {
               <div className="thumb-icon" aria-hidden="true"><IoNotificationsOutline /></div>
               <div>
                 {notif.link ? (
-                  <Link to={notif.link} className="result-link">
+                  <Link 
+                    to={notif.link} 
+                    className="result-link"
+                    onClick={() => handleMarkAsRead(notif.notification_id)}
+                  >
                     <div className="item-title">{notif.title}</div>
                   </Link>
                 ) : (
